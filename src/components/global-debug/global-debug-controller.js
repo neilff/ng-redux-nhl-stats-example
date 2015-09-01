917bc8059b46c9973b9681426d7ecec62959d1cc
Neil Fenton <neilff@gmail.com>
2015-09-01 18:54:58 -0400

    added all state to global debug

--- src/components/global-debug/global-debug-controller.js
+++ src/components/global-debug/global-debug-controller.js
@@ -2,10 +2,7 @@ export default class GlobalDebugController {
   constructor($scope, $ngRedux) {
     $ngRedux.connect($scope, state => {
       return {
-        globalState: {
-          router: state.router,
-          filter: state.filter,
-        }
+        globalState: state
       };
     });
   }
